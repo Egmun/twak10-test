586e7eb54825c04343cea8e56bc7d18fa26a0bd3
Gustav Sköldebrink <gurkmaster@hotmail.se>
2020-04-22 11:44:41 +0200

    Update studentlist plz

--- studentlistapp.py
+++ studentlistapp.py
@@ -1,4 +1,4 @@
-studentlist = ["johson", "xxyy0011", "joso1122"]
+studentlist = ["johson", "xxyy0011", "joso1122", "skgu17e3", "amde1798"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
